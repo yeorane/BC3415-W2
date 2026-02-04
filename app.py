db1c1851c77f38e8a2f4f6568b00664e1acca7d9
yeorane <yeorrane@gmail.com>
2026-02-04 08:02:09 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template,request
+import joblib
+
+model = joblib.load("DBS_SGD.pkl")
 
 app = Flask(__name__)
 
@@ -10,5 +13,16 @@ def index():
 def main():
     return(render_template("main.html"))
 
+@app.route("/dbs", methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+@app.route("/dbsPrediction", methods=["GET","POST"])
+def dbsPrediction():
+    q = float(request.form.get("q"))
+    r = model.predict([[q]])
+    r = r [0][0]
+    return(render_template("dbsPrediction.html",r=r))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=1111)
